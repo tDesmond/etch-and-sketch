19081bafa1740586d7971523fcaf86dc6d5c62e8
tDesmond <timmydesmond95@gmail.com>
2022-09-21 16:38:49 +0100

    Added a prompt allowing the user to select grid size.

--- app.js
+++ app.js
@@ -2,27 +2,34 @@
 const DRAWING_CONTAINER = document.querySelector(".drawing-container")
 const GRID_SQUARE = document.createElement('div')
 const DRAW_ERASE_SELECTOR = document.querySelectorAll('input[type="radio"]') 
-const GRID_SIZE = 8
+const GRID_SIZE_BUTTON = document.querySelector('#grid-size-btn') 
 
+let gridSize = 32
 let drawColor = "black"
+
+let clearGid = () => DRAWING_CONTAINER.innerHTML =''
 
 
 function drawBoxFormat(){
     console.log(DRAWING_CONTAINER.offsetWidth)
     let drawAreaSize = DRAWING_CONTAINER.offsetWidth
-    drawAreaSize -= drawAreaSize % GRID_SIZE
+    drawAreaSize -= drawAreaSize % gridSize
 
     DRAWING_CONTAINER.style.width = `${drawAreaSize}px`
     DRAWING_CONTAINER.style.height = `${drawAreaSize}px`
 
-    let gridBoxSize = drawAreaSize / GRID_SIZE
+    let gridBoxSize = drawAreaSize / gridSize
 
     GRID_SQUARE.style.width = `${gridBoxSize}px`
     GRID_SQUARE.style.height = `${gridBoxSize}px`
 }
 
 function generateGrid(){
-    for(let i = 0; i < (GRID_SIZE**2); i++){
+    drawBoxFormat()
+    clearGid()
+    
+    console.log(gridSize)
+    for(let i = 0; i < (gridSize**2); i++){
         let node = GRID_SQUARE.cloneNode(true)
         node.setAttribute('data-id', i)
 
@@ -47,9 +54,12 @@ function checkDrawOrErase(){
         }   
     }
 }
-
-drawBoxFormat()
 generateGrid()
+
+GRID_SIZE_BUTTON.onclick = () => {
+    gridSize = prompt("Change grid size?")
+    generateGrid()
+}
 
 
 
